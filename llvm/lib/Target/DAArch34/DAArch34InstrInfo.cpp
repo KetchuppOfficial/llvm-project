4b7e023e2ed1c26e95c0fae2f5ec81ce938c2238
KetchuppOfficial <dolgov04@list.ru>
2025-03-29 01:31:58 +0300

    [DAArch34] Add InstrInfo and TSInfo to DAArch34Subtarget

--- llvm/lib/Target/DAArch34/DAArch34InstrInfo.cpp
+++ llvm/lib/Target/DAArch34/DAArch34InstrInfo.cpp
@@ -1,5 +1,8 @@
 #include "DAArch34InstrInfo.h"
 
+#define GET_INSTRINFO_CTOR_DTOR
+#include "DAArch34GenInstrInfo.inc"
+
 namespace llvm {
 
 DAArch34InstrInfo::DAArch34InstrInfo() : DAArch34GenInstrInfo() {}
