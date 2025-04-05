3f177bef10f4a1bb1669694dc663b824bfc42e71
KetchuppOfficial <dolgov04@list.ru>
2025-04-05 23:06:25 +0300

    [DAArch34] Add support of intrinsics as clang built-ins

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -366,12 +366,23 @@ namespace clang {
     };
   }
 
+  namespace DAArch34 {
+    enum {
+      LastTIBuiltin = clang::Builtin::FirstTSBuiltin - 1,
+      #define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+      #include "clang/Basic/BuiltinsDAArch34.def"
+      #undef BUILTIN
+      LastTSBuiltin
+    };
+  } // end namespace DAArch34
+
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin,
+       DAArch34::LastTSBuiltin});
 
 } // end namespace clang.
 
--- clang/lib/Basic/Targets/DAArch34.cpp
+++ clang/lib/Basic/Targets/DAArch34.cpp
@@ -1,16 +1,29 @@
+#include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 #include "DAArch34.h"
 
-namespace clang::targets {
+namespace clang {
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, LanguageID::ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsDAArch34.def"
+#undef BUILTIN
+};
+
+namespace targets {
 
 void DAArch34TargetInfo::getTargetDefines(const LangOptions &Opts,
                                           MacroBuilder &Builder) const {
-    Builder.defineMacro("__daarch34__");
+  Builder.defineMacro("__daarch34__");
 }
 
 llvm::ArrayRef<Builtin::Info> DAArch34TargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo, DAArch34::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
 
-} // end namespace clang::targets
+} // end namespace targets
+
+} // end namespace clang
